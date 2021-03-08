553835c1a61f3827bf95c8dd99efd4e2c4df1979
miguel <miguel.ferreira>
2021-03-08 19:58:45 +0000

    cors

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 var app = require('http').createServer();
 
-//CORS TRIALS
- var app = require('http').createServer(function(req,res){
- 	// Set CORS headers
- 	res.setHeader('Access-Control-Allow-Origin', 'http://dad.p6.dev');
- 	res.setHeader('Access-Control-Request-Method', '*');
- 	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
- 	res.setHeader('Access-Control-Allow-Credentials', true);
- 	res.setHeader('Access-Control-Allow-Headers', req.header.origin);
- 	if ( req.method === 'OPTIONS' ) {
- 		res.writeHead(200);
- 		res.end();
- 		return;
- 	}
+var io = require('socket.io')(app, {
+	cors: {
+		origin: "*"
+	}
 });
-
-var io = require('socket.io')(app);
 
 app.listen(5000, function(){
 	console.log('listening on *:5000');
@@ -23,7 +12,7 @@ app.listen(5000, function(){
 
 io.on('connection', function (socket) {
 	console.log('client connected')
-	
+
 	socket.on('join_room', function (data) {
 		console.log(data)
 		socket.join(data.room_id)
